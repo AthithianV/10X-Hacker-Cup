361d8baf804f56e357d08875f2019de7da422292
Athithian <athithianpkt2001@gmail.com>
2024-12-10 12:11:40 +0530

    introtalk fronent initiated with vite

---
+++ intro-talk-backend/src/database/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA7932kRvtnwWPhXLyZ8IN30alnvrq6CAA",
+  authDomain: "introtalk-d1653.firebaseapp.com",
+  projectId: "introtalk-d1653",
+  storageBucket: "introtalk-d1653.firebasestorage.app",
+  messagingSenderId: "19562086941",
+  appId: "1:19562086941:web:93b69bd249169b9ff3bcf4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ introtalk/src/App.tsx
@@ -0,0 +1,10 @@
+import './App.css'
+
+function App() {
+
+  return (
+      <h1 className='text-center text-4xl text-red-500'>Welcome to IntroTalk</h1>
+  )
+}
+
+export default App;
